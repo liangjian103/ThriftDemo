52537ec21c60136209b04a41a4db0b4485ee5713
LiangJian <liangjian5858@qq.com>
2014-06-26 21:37:25 +0800

    使用本地的方式调试.

--- testStorm/src/testStorm/WordcountStart.java
+++ testStorm/src/testStorm/WordcountStart.java
@@ -1,7 +1,7 @@
 package testStorm;
 
 import backtype.storm.Config;
-import backtype.storm.StormSubmitter;
+import backtype.storm.LocalCluster;
 import backtype.storm.generated.AlreadyAliveException;
 import backtype.storm.generated.InvalidTopologyException;
 import backtype.storm.topology.TopologyBuilder;
@@ -19,22 +19,18 @@ public class WordcountStart {
 		conf.setDebug(true);
 		conf.setNumWorkers(20);
 
-		// LocalCluster cluster = new LocalCluster();
-		// cluster.submitTopology("wordcount-demo", conf, builder.createTopology());
+		 LocalCluster cluster = new LocalCluster();
+		 cluster.submitTopology("wordcount-demo", conf, builder.createTopology());
 		try {
-			StormSubmitter.submitTopology("wordcount-demo", conf, builder.createTopology());
+			cluster.submitTopology("wordcount-demo", conf, builder.createTopology());
 			Thread.sleep(10000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (AlreadyAliveException e) {
-			e.printStackTrace();
-		} catch (InvalidTopologyException e) {
-			e.printStackTrace();
 		}
 
-		// cluster.killTopology("wordcount-demo");
-		// cluster.shutdown();
+		 cluster.killTopology("wordcount-demo");
+		 cluster.shutdown();
 
 	}
 
